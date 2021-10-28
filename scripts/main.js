45a4b22f5d51996ce1ea9626a8dadcf4fba9a3c0
Josh Evans <88986229+Jevans1441@users.noreply.github.com>
2021-10-28 19:59:28 -0400

    added event listener

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,29 @@
-window.addEventListener('DOMContentLoaded', function() {
+window.addEventListener("DOMContentLoaded", function () {
   // Execute after page load
-})
+
+  //deal btn start
+  const dealBtn = document.querySelector("#deal-button");
+  dealBtn.addEventListener("click", (e) => {
+    // Add new card element
+    const dealerHand = document.querySelector("#dealer-hand");
+    const newCard = document.createElement("img");
+    img = "images/2_of_clubs.png";
+    dealerHand.appendChild(newCard);
+  });
+
+  //deal btn end
+
+  //hit btn start
+  const hitBtn = document.querySelector("#hit-button");
+  hitBtn.addEventListener("click", (e) => {
+    console.log("Hit");
+  });
+  //hit btn end
+
+  //stand btn start
+  const standBtn = document.querySelector("#stand-button");
+  standBtn.addEventListener("click", (e) => {
+    console.log("Stand");
+  });
+  //stand btn end
+});
