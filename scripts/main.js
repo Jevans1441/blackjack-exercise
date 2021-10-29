36717ed011f482f160d5e3dde2d493f8d87f7a38
Josh Evans <88986229+Jevans1441@users.noreply.github.com>
2021-10-28 21:25:14 -0400

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -6,17 +6,27 @@ window.addEventListener("DOMContentLoaded", function () {
   dealBtn.addEventListener("click", (e) => {
     // Add new card element
     const dealerHand = document.querySelector("#dealer-hand");
+    const playerHand = document.querySelector("#player-hand");
     const newCard = document.createElement("img");
-    img = "images/2_of_clubs.png";
+    const newCard2 = document.createElement("img");
+    newCard.src = "images/2_of_hearts.png";
+    newCard2.src = "images/3_of_hearts.png";
     dealerHand.appendChild(newCard);
+    playerHand.appendChild(newCard2);
   });
-
   //deal btn end
 
   //hit btn start
   const hitBtn = document.querySelector("#hit-button");
   hitBtn.addEventListener("click", (e) => {
-    console.log("Hit");
+    const dealerHand = document.querySelector("#dealer-hand");
+    const playerHand = document.querySelector("#player-hand");
+    const newCard = document.createElement("img");
+    const newCard2 = document.createElement("img");
+    newCard.src = "images/2_of_hearts.png";
+    newCard2.src = "images/3_of_hearts.png";
+    dealerHand.appendChild(newCard);
+    playerHand.appendChild(newCard2);
   });
   //hit btn end
 
@@ -27,3 +37,14 @@ window.addEventListener("DOMContentLoaded", function () {
   });
   //stand btn end
 });
+
+//Build Deck
+function buildDeck() {
+  const suites = ["spades", "diamonds", "clubs", "hearts"];
+  const deck = [];
+
+  for (i = 1; i <= suites.length; i++) {
+    
+  }
+  console.log(deck);
+}
